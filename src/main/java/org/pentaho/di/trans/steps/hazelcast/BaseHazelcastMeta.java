7c22fce275f526c4c4f1957d0bc9ba8639a8b4dc
János Veres <janos@featme.info>
2016-12-29 23:59:16 +0100

    bump kettle and metastore dependencies to r7.0, hazelcast to r3.7 intellij ignore add hazelcast groupConfig [name, password] to HazelcastInputMeta and HazelcastOutputMeta adding group config labels to resources pulling up common members/code to BaseHazelcastMeta

--- src/main/java/org/pentaho/di/trans/steps/hazelcast/BaseHazelcastMeta.java
+++ src/main/java/org/pentaho/di/trans/steps/hazelcast/BaseHazelcastMeta.java
@@ -20,10 +20,17 @@
 
 package org.pentaho.di.trans.steps.hazelcast;
 
+import org.pentaho.di.core.Const;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.xml.XMLHandler;
+import org.pentaho.di.repository.ObjectId;
+import org.pentaho.di.repository.Repository;
 import org.pentaho.di.trans.step.BaseStepMeta;
 import org.pentaho.di.trans.step.StepMetaInterface;
+import org.w3c.dom.Node;
 
 import java.net.InetSocketAddress;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -40,4 +47,47 @@ public abstract class BaseHazelcastMeta extends BaseStepMeta implements StepMeta
   public void setServers( Set<InetSocketAddress> servers ) {
     this.servers = servers;
   }
+
+    protected void writeServersToXml(StringBuffer xml, Set<InetSocketAddress> servers) {
+        if (servers != null) {
+            for (InetSocketAddress addr : servers) {
+                xml.append("      <server>").append(Const.CR);
+                xml.append("        ").append(XMLHandler.addTagValue("hostname", addr.getHostName()));
+                xml.append("        ").append(XMLHandler.addTagValue("port", addr.getPort()));
+                xml.append("      </server>").append(Const.CR);
+            }
+        }
+    }
+
+    protected void readServersFromXml(Node serverNodes, int nrservers) {
+        for (int i = 0; i < nrservers; i++) {
+            Node fnode = XMLHandler.getSubNodeByNr(serverNodes, "server", i);
+
+            String hostname = XMLHandler.getTagValue(fnode, "hostname");
+            int port = Integer.parseInt(XMLHandler.getTagValue(fnode, "port"));
+            servers.add(new InetSocketAddress(hostname, port));
+        }
+    }
+
+    protected void readServerFromRep(Repository rep, ObjectId id_step, int nrservers) throws KettleException {
+        for (int i = 0; i < nrservers; i++) {
+            servers.add(new InetSocketAddress(rep.getStepAttributeString(id_step, i, "hostname"), Integer.parseInt(rep
+                    .getStepAttributeString(id_step, i, "port"))));
+        }
+    }
+
+    public void allocate(int nrfields) {
+        servers = new HashSet<InetSocketAddress>(nrfields);
+    }
+
+    protected void writeServerToRep(Repository rep, ObjectId id_transformation, ObjectId id_step) throws KettleException {
+        int i = 0;
+        Set<InetSocketAddress> servers = this.getServers();
+        if (servers != null) {
+            for (InetSocketAddress addr : servers) {
+                rep.saveStepAttribute(id_transformation, id_step, i++, "hostname", addr.getHostName());
+                rep.saveStepAttribute(id_transformation, id_step, i++, "port", addr.getPort());
+            }
+        }
+    }
 }
--- src/main/java/org/pentaho/di/trans/steps/hazelcast/HazelcastOutput.java
+++ src/main/java/org/pentaho/di/trans/steps/hazelcast/HazelcastOutput.java
@@ -20,110 +20,115 @@
 
 package org.pentaho.di.trans.steps.hazelcast;
 
-import java.net.InetSocketAddress;
-import java.util.Set;
-
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.ClientNetworkConfig;
+import com.hazelcast.config.GroupConfig;
+import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IQueue;
+import org.pentaho.di.core.Const;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.i18n.BaseMessages;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
-import org.pentaho.di.trans.step.BaseStep;
-import org.pentaho.di.trans.step.StepDataInterface;
-import org.pentaho.di.trans.step.StepInterface;
-import org.pentaho.di.trans.step.StepMeta;
-import org.pentaho.di.trans.step.StepMetaInterface;
+import org.pentaho.di.trans.step.*;
 
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.client.config.ClientNetworkConfig;
-import com.hazelcast.core.HazelcastInstance;
+import java.net.InetSocketAddress;
+import java.util.Set;
 
 /**
  * The Hazelcast Output step stores value objects, for the given key names, to Hazelcast server(s).
  */
 public class HazelcastOutput extends BaseStep implements StepInterface {
-  private static Class<?> PKG = HazelcastOutputMeta.class; // for i18n purposes, needed by Translator2!! $NON-NLS-1$
+    private static Class<?> PKG = HazelcastOutputMeta.class; // for i18n purposes, needed by Translator2!! $NON-NLS-1$
 
-  protected HazelcastOutputMeta meta;
-  protected HazelcastOutputData data;
+    protected HazelcastOutputMeta meta;
+    protected HazelcastOutputData data;
 
-  protected HazelcastInstance client = null;
+    protected HazelcastInstance client = null;
 
-  public HazelcastOutput( StepMeta stepMeta, StepDataInterface stepDataInterface, int copyNr, TransMeta transMeta,
-                          Trans trans ) {
-    super( stepMeta, stepDataInterface, copyNr, transMeta, trans );
-  }
+    public HazelcastOutput(StepMeta stepMeta, StepDataInterface stepDataInterface, int copyNr, TransMeta transMeta,
+                           Trans trans) {
+        super(stepMeta, stepDataInterface, copyNr, transMeta, trans);
+    }
 
-  @Override
-  public boolean init( StepMetaInterface smi, StepDataInterface sdi ) {
-    if ( super.init( smi, sdi ) ) {
-      try {
-        // Create client and connect to Hazelcast server(s)
-        Set<InetSocketAddress> servers = ( (HazelcastOutputMeta) smi ).getServers();
-        ClientConfig clientConfig = new ClientConfig();
-        ClientNetworkConfig clientNetConfig = new ClientNetworkConfig();
-        if ( servers != null ) {
+    @Override
+    public boolean init(StepMetaInterface smi, StepDataInterface sdi) {
+        if (super.init(smi, sdi)) {
+            try {
+                // Create client and connect to Hazelcast server(s)
+                HazelcastOutputMeta outputMeta = (HazelcastOutputMeta) smi;
+                Set<InetSocketAddress> servers = outputMeta.getServers();
+                ClientConfig clientConfig = new ClientConfig();
+                ClientNetworkConfig clientNetConfig = new ClientNetworkConfig();
+                if (servers != null) {
 
-          for ( InetSocketAddress server : servers ) {
-            clientNetConfig.addAddress( server.getHostName() + ":" + server.getPort() );
-          }
-          clientConfig.setNetworkConfig( clientNetConfig );
-          client = HazelcastClient.newHazelcastClient( clientConfig );
+                    for (InetSocketAddress server : servers) {
+                        clientNetConfig.addAddress(server.getHostName() + ":" + server.getPort());
+                    }
+                    clientConfig.setNetworkConfig(clientNetConfig);
+
+                    // if there's a group config, apply it
+                    if (!Const.isEmpty(outputMeta.getGroupName())) {
+                        GroupConfig grpCfg = new GroupConfig(outputMeta.getGroupName(), outputMeta.getGroupPassword());
+                        clientConfig.setGroupConfig(grpCfg);
+                    }
+
+                    client = HazelcastClient.newHazelcastClient(clientConfig);
+                }
+                return true;
+            } catch (Exception e) {
+                logError(BaseMessages.getString(PKG, "HazelcastOutput.Error.ConnectError"), e);
+                return false;
+            }
+        } else {
+            return false;
         }
+    }
+
+    public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException {
+        meta = (HazelcastOutputMeta) smi;
+        data = (HazelcastOutputData) sdi;
+
+        Object[] r = getRow(); // get row, set busy!
+
+        // If no more input to be expected, stop
+        if (r == null) {
+            setOutputDone();
+            return false;
+        }
+
+        if (first) {
+            first = false;
+
+            // clone input row meta for now, we will change it (add or set inline) later
+            data.outputRowMeta = getInputRowMeta().clone();
+            // Get output field types
+            meta.getFields(data.outputRowMeta, getStepname(), null, null, this, repository, metaStore);
+        }
+
+        Object[] outputRowData = r;
+
+        // Get structure from Hazelcast
+        String structName = fieldSubstitute(environmentSubstitute(meta.getStructureName()), getInputRowMeta(), r);
+        String structType = meta.getStructureType();
+        // TODO better way to pick which API method to call based on structure type
+        if (structType == "Queue") {
+            IQueue<SerializableRow> q = client.getQueue(structName);
+            SerializableRow sRow = new SerializableRow(data.outputRowMeta, outputRowData);
+            if (!q.offer(sRow)) {
+                putError(data.outputRowMeta, outputRowData, 1,
+                        BaseMessages.getString(PKG, "HazelcastOutput.Error.Row.PutRowInQ"), null, null);
+            }
+        }
+        putRow(data.outputRowMeta, outputRowData); // copy row to possible alternate rowset(s).
+
+        if (checkFeedback(getLinesRead())) {
+            if (log.isBasic()) {
+                logBasic(BaseMessages.getString(PKG, "HazelcastOutput.Log.LineNumber") + getLinesRead());
+            }
+        }
+
         return true;
-      } catch ( Exception e ) {
-        logError( BaseMessages.getString( PKG, "HazelcastOutput.Error.ConnectError" ), e );
-        return false;
-      }
-    } else {
-      return false;
     }
-  }
-
-  public boolean processRow( StepMetaInterface smi, StepDataInterface sdi ) throws KettleException {
-    meta = (HazelcastOutputMeta) smi;
-    data = (HazelcastOutputData) sdi;
-
-    Object[] r = getRow(); // get row, set busy!
-
-    // If no more input to be expected, stop
-    if ( r == null ) {
-      setOutputDone();
-      return false;
-    }
-
-    if ( first ) {
-      first = false;
-
-      // clone input row meta for now, we will change it (add or set inline) later
-      data.outputRowMeta = getInputRowMeta().clone();
-      // Get output field types
-      meta.getFields( data.outputRowMeta, getStepname(), null, null, this, repository, metaStore );
-    }
-
-    Object[] outputRowData = r;
-
-    // Get structure from Hazelcast
-    String structName = fieldSubstitute( environmentSubstitute( meta.getStructureName() ), getInputRowMeta(), r );
-    String structType = meta.getStructureType();
-    // TODO better way to pick which API method to call based on structure type
-    if ( structType == "Queue" ) {
-      IQueue<SerializableRow> q = client.getQueue( structName );
-      SerializableRow sRow = new SerializableRow( data.outputRowMeta, outputRowData );
-      if ( !q.offer( sRow ) ) {
-        putError( data.outputRowMeta, outputRowData, 1,
-          BaseMessages.getString( PKG, "HazelcastOutput.Error.Row.PutRowInQ" ), null, null );
-      }
-    }
-    putRow( data.outputRowMeta, outputRowData ); // copy row to possible alternate rowset(s).
-
-    if ( checkFeedback( getLinesRead() ) ) {
-      if ( log.isBasic() ) {
-        logBasic( BaseMessages.getString( PKG, "HazelcastOutput.Log.LineNumber" ) + getLinesRead() );
-      }
-    }
-
-    return true;
-  }
 }
